c2892bfbc7d512ce77ecc723b7a9024e305da328
jiang <jiang@ipride.co.jp>
2018-04-04 00:41:13 +0900

    公開すべきではない文書を削除

--- 01_結合テスト/TestJavaSource/Excat_V3_IT_Packages/src/packages/p03/p0301/p030101/C030101.java
+++
@@ -1,7 +0,0 @@
-package packages.p03.p0301.p030101;
-
-public class C030101 {
-	public void throwException() throws Exception {
-		throw new Exception("C030101");
-	}
-}
--- 01_結合テスト/TestJavaSource/Excat_V3_IT_Packages/src/packages/p07/C07.java
+++
@@ -1,7 +0,0 @@
-package packages.p07;
-
-public class C07 {
-	public void throwException() throws Exception {
-		throw new Exception("C07");
-	}
-}
